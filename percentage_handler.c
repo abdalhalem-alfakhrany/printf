99e813544056c1806f10de8d9ab9dc1b9291c927
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-04-27 00:45:08 +0200

    percentag_handler.c filed updated(bettyfied)

--- percentage_handler.c
+++ percentage_handler.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-int percentage_handler(char *str,va_list *list)
+/**
+ * percentage_handler - handel tow percentage signes
+ * @str: string buffer
+ * @list: variadic arguments
+ * Return: the size of chars added
+ */
+int percentage_handler(char *str, va_list *list)
 {
 	(void) list;
 	*str = '%';
