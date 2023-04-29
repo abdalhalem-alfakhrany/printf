2d9775def621ad5264b259f6f087fe5207579d22
El-Zieny <abdoelzieny72@gmail.com>
2023-04-29 03:23:51 +0300

    printf percentage  files updated

--- _printf.c
+++ _printf.c
@@ -11,11 +11,10 @@
 int handel_specifires(va_list *list, const char *format, char *str, int *size)
 {
 	int i = 0, j = 0, result = 1;
-	specifier_handler handlers[] = {
-		{'c', c_handler}, {'s', s_handler}, {'d', d_handler},
-		{'i', i_handler}, {'b', b_handler}, {'r', U_handler},
-		{'x', U_handler}, {'X', U_handler}, {'o', U_handler},
-		{'u', U_handler}, {'S', c_handler}};
+	specifier_handler handlers[] = {{'c', c_handler}, {'s', s_handler},
+		{'d', d_handler}, {'i', i_handler}, {'b', b_handler},
+		{'r', U_handler},{'x', U_handler}, {'X', U_handler},
+		{'o', U_handler},{'u', U_handler}, {'S', c_handler}};
 
 	if (!format || !list || !str)
 		return (-1);
@@ -28,7 +27,6 @@ int handel_specifires(va_list *list, const char *format, char *str, int *size)
 			continue;
 		}
 		result = percentage_handler(&str[*size], &format[i]);
-
 		if (result == -1)
 			return (-1);
 		else if (result == 1)
